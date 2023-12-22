c5a6cd24deaeb5c14ab308719c344b338ebc6c9c
Vuyisile Ndlovu <terrameijar@gmail.com>
2023-12-22 16:30:35 +0200

    small edit to trigger build

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye there.
+          Deployed automatically from CicleCI
         </p>
         <a
           className="App-link"
