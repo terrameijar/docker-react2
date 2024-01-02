bfa223a60e14900017662e1558c081c572656744
Vuyisile Ndlovu <terrameijar@gmail.com>
2024-01-02 12:04:07 +0200

    Small change to test circleci

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Deployed automatically from CicleCI
+          Deployed automatically from CicleCI.
         </p>
         <a
           className="App-link"
